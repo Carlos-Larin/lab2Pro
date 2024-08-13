15866ee205ee39566b019c0922c93562188238d0
Carlos-Larin <nekokar701@gmail.com>
2024-08-12 20:26:30 -0600

    first commit

---
+++ lab2.py
@@ -0,0 +1,12 @@
+class Libro():
+    tamaño=""
+    paginas=0
+    precioVenta=0
+    precioXPag=0
+    pesoXPag=0
+    titulo=""
+    tapa=""
+    URL=""
+    ISBN=""
+    Estado="No Hay Registros del Libro"
+    #holaZapata estuvo aqui

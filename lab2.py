48225e5dd12cd5c5c8bf22b1579a362beba3685a
Carlos-Larin <nekokar701@gmail.com>
2024-08-12 21:15:32 -0600

    parte2

--- lab2.py
+++ lab2.py
@@ -10,3 +10,11 @@ class Libro():
     ISBN=""
     Estado="No Hay Registros del Libro"
     #holaZapata estuvo aqui
+    def __init__(self):
+        self.tamaño="17x24"
+        self.precioXPag=300
+        self.pesoXPag=0.8
+
+    def RegistraLibro(self):
+        Estado="Registrado"
+        return Estado
